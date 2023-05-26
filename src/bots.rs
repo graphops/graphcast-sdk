f1c14ed26221069e5f10385a3eba0f9ccb9d97b5
axiomatic-aardvark <pavlovskipetko@abv.bg>
2023-05-26 15:29:19 +0300

    feat: add telegram notifications

--- src/bots.rs
+++ src/bots.rs
@@ -1,9 +1,13 @@
 use std::collections::HashMap;
 use std::error::Error;
+use teloxide::types::ParseMode;
 use thiserror::Error;
 
 use rsb_derive::Builder;
 use slack_morphism::prelude::*;
+
+use teloxide::prelude::*;
+use teloxide::types::ChatId;
 
 // DiscordBot
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -60,6 +64,45 @@ impl SlackBot {
         );
         let post_chat_resp = session.chat_post_message(&post_chat_req).await?;
         Ok(post_chat_resp)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TelegramBot {
+    bot: Bot,
+}
+
+#[derive(Error, Debug)]
+pub enum TelegramBotError {
+    #[error("Request error: {0}")]
+    RequestError(#[from] teloxide::RequestError),
+}
+
+impl TelegramBot {
+    pub fn new(bot_token: String) -> Self {
+        Self {
+            bot: Bot::new(bot_token),
+        }
+    }
+
+    pub async fn send_message(
+        &self,
+        chat_id: i64,
+        radio_name: &str,
+        content: &str,
+    ) -> Result<(), TelegramBotError> {
+        let message = format!(
+            "🚨 Notification from Radio '{radio_name}' \n{content}",
+            radio_name = radio_name,
+            content = content
+        );
+        self.bot
+            .send_message(ChatId(chat_id), message)
+            .parse_mode(ParseMode::Html) // or ParseMode::MarkdownV2
+            .disable_web_page_preview(true)
+            .send()
+            .await?;
+        Ok(())
     }
 }
 
