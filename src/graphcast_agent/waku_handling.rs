8b6495484d94d984630c243c359b2f95f19ce70b
hopeyen <hopey1118@gmail.com>
2023-07-18 14:55:17 -0500

    refactor: generic message validation

--- src/graphcast_agent/waku_handling.rs
+++ src/graphcast_agent/waku_handling.rs
@@ -1,3 +1,4 @@
+use prost::Message;
 use std::{borrow::Cow, env, num::ParseIntError, sync::Arc};
 use std::{collections::HashSet, time::Duration};
 use std::{net::IpAddr, str::FromStr};
@@ -11,6 +12,9 @@ use waku::{
 };
 
 use crate::{app_name, cf_nameserver, discovery_url, graphql::QueryError};
+
+use super::message_typing::GraphcastMessage;
+use super::{message_typing, GraphcastAgent};
 
 pub const SDK_VERSION: &str = "0";
 
@@ -435,6 +439,34 @@ pub async fn handle_signal(
         _ => Err(WakuHandlingError::InvalidMessage(format!(
             "Unrecognized signal!\n {:?}",
             serde_json::to_string(&signal)
+        ))),
+    }
+}
+
+/// Parse and validate incoming message
+pub async fn generic_graphcast_check<
+    T: Message
+        + ethers::types::transaction::eip712::Eip712
+        + Default
+        + Clone
+        + 'static
+        + async_graphql::OutputType,
+>(
+    message: WakuMessage,
+    graphcast_agent: &GraphcastAgent,
+) -> Result<GraphcastMessage<T>, WakuHandlingError> {
+    match <message_typing::GraphcastMessage<T> as Message>::decode(message.payload()) {
+        Ok(graphcast_message) => message_typing::check_message_validity(
+            graphcast_message,
+            &graphcast_agent.nonces,
+            graphcast_agent.callbook.clone(),
+            graphcast_agent.graphcast_identity.graphcast_id.clone(),
+            &graphcast_agent.id_validation,
+        )
+        .await
+        .map_err(|e| WakuHandlingError::InvalidMessage(e.to_string())),
+        Err(e) => Err(WakuHandlingError::InvalidMessage(format!(
+            "Waku message not interpretated as a Graphcast message\nError occurred: {e:?}"
         ))),
     }
 }
